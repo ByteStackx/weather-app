87a3d6ec0564ca24ff73faa51d681c07253db68b
Ashley <ashleymanchidi@gmail.com>
2025-08-26 19:14:25 +0200

    Implemented search
    You can now search the weather of any city by putting in the name.

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -11,3 +11,15 @@ export async function fetchCurrentWeather(lat: number, lon: number) {
   if (!res.ok) throw new Error("Failed to fetch weather data");
   return res.json();
 }
+
+export async function fetchWeatherByCity(city: string) {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  if (!API_KEY) throw new Error("API key missing");
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+  );
+
+  if (!res.ok) throw new Error("City not found or API error");
+  return res.json();
+}
